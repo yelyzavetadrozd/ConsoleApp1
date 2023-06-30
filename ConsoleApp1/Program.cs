1ae95039b3a9a5e4bbeb308e9b1943aa29240e1b
Yelyzaveta Drozd <elizavetadrozd266@gmail.com>
2023-06-30 12:11:28 +0300

    delete 2 lines

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,9 +10,6 @@
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
-
-
-         
         }
     }
 }
